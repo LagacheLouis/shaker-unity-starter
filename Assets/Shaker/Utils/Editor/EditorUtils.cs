a29f77859b4bf729146032b77c40327c928f8394
Lagache Louis <louislagache@gmail.com>
2020-07-14 01:23:47 +0200

    stillwwater

--- Assets/Shaker/Utils/Editor/EditorUtils.cs
+++ Assets/Shaker/Utils/Editor/EditorUtils.cs
@@ -27,6 +27,41 @@ namespace Shaker
             AssetDatabase.OpenAsset(obj);
         }
 
+        [MenuItem("Shaker/Open Terminal")]
+        static void OpenTerminal()
+        {
+
+            if (Application.platform == RuntimePlatform.WindowsEditor)
+            {
+                System.Diagnostics.Process.Start("CMD.exe", "cd " + GetProjectPath());
+            }
+            else if (Application.platform == RuntimePlatform.OSXEditor)
+            {
+                System.Diagnostics.Process.Start(@"/Applications/Utilities/Terminal.app/Contents/MacOS/Terminal", "cd " + GetProjectPath());
+            }
+            else
+            {
+                Debug.LogWarning("Terminal shortcut is not supported on your plateform");
+            }
+        }
+
+        public static string GetProjectPath()
+        {
+            string path = Application.dataPath;
+            path = path.Replace("/Assets", "");
+            return path;
+        }
+
+#if UNITY_EDITOR_OSX
+    [MenuItem("Shaker/Reveal in Finder")]
+#else
+        [MenuItem("Shaker/Show in Explorer")]
+#endif
+        static void OpenInFileExplorer()
+        {
+            EditorUtility.RevealInFinder(GetProjectPath());
+        }
+
     }
 }
 
